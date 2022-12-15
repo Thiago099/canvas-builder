591b99f2314882987ef98da0bbb6be03a825bc98
Thiago Kaique <thiago.kaique@stayfilm.com.br>
2022-12-15 19:51:57 -0300

    [feature]

--- src/main.js
+++ src/main.js
@@ -6,23 +6,19 @@ import { canvas,surface, fillerSurface } from "./canvas-builder/canvas-builder";
 
 function makeDraggable(surface)
 {
-  var x = surface.x;
-  var y = surface.y;
-  var w = surface.w;
-  var h = surface.h;
   var dx;
   var dy;
   function drag(e)
   {
-    x = e.offsetX - dx;
-    y = e.offsetY - dy;
-    surface.position(x,y);
+    surface.x = e.offsetX - dx;
+    surface.y = e.offsetY - dy;
+    surface.position(surface.x,surface.y);
     surface.update()
   }
   document.addEventListener("mousedown", function(e) {
-    if(e.offsetX < x || e.offsetX > x + w || e.offsetY < y || e.offsetY > y + h) return
-    dx = e.offsetX - x;
-    dy = e.offsetY - y;
+    if(e.offsetX < surface.x || e.offsetX > surface.x + surface.w || e.offsetY < surface.y || e.offsetY > surface.y + surface.h) return
+    dx = e.offsetX - surface.x;
+    dy = e.offsetY - surface.y;
     document.addEventListener("mousemove", drag);
     function dragend(e) {
       document.removeEventListener("mousemove", drag);
@@ -34,10 +30,11 @@ function makeDraggable(surface)
 var myc = canvas(cel);
 
 
-function node(node, text)
+function node(text)
 {
-  node.style(
-  function(ctx,w,h)
+  var result = 
+  surface()
+  .style(function(ctx,w,h)
   {
     ctx.fillStyle = "#0077cc";
     ctx.beginPath();
@@ -52,7 +49,8 @@ function node(node, text)
 
     connection.update();
   })
-  makeDraggable(node1)
+  makeDraggable(result)
+  return result;
 }
 
 
@@ -67,86 +65,16 @@ var connection = fillerSurface(myc)
   ctx.stroke();
 })
 
-var node1 = surface()
+const node1 = node("Node 1")
 .parent(myc)
 .position(25,25)
 .size(100,50)
 
-var node2 = surface()
+const node2 = node("Node 2")
 .parent(myc)
 .position(125+30,25)
 .size(100,50)
 
-node(node1,"Node 1")
-node(node2,"Node 2")
 
-makeDraggable(node2);
 
 myc.update();
-/*
-var x = 25;
-var y = 25;
-
-
-
-var dx;
-var dy;
-
-function drag(e)
-{
-  x = e.clientX - dx;
-  y = e.clientY - dy;
-  update()
-}
-
-document.addEventListener("mousedown", function(e) {
-  dx = e.clientX - x;
-  dy = e.clientY - y;
-  document.addEventListener("mousemove", drag);
-  function dragend(e) {
-    document.removeEventListener("mousemove", drag);
-    document.removeEventListener("mouseup", dragend);
-  }
-  document.addEventListener("mouseup", dragend);
-});
-
-update()
-function update()
-{
-  // ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-  // // draw a rectangle to represent the first step in the flowchart
-  // ctx.fillStyle = "#0077cc";
-  // ctx.beginPath();
-  // ctx.roundRect(x, y, 150, 100,5);
-  // ctx.fill();
-  // // add text to the rectangle
-  // ctx.font = "18px sans-serif";
-  // ctx.fillStyle = "white";
-  // ctx.textAlign = "center";
-  // ctx.textBaseline = "middle";
-  // ctx.fillText("Step 1", x+100-25, y+75-25);
-  
-  // // draw an arrow to the next step
-  // ctx.beginPath();
-  // ctx.moveTo(175, 75);
-  // ctx.lineTo(275, 75);
-  // ctx.strokeStyle = "white";
-  // ctx.lineWidth = 5;
-  // ctx.stroke();
-  
-  // // draw a circle to represent the second step in the flowchart
-  // ctx.beginPath();
-  // ctx.arc(325, 75, 50, 0, 2 * Math.PI);
-  // ctx.fillStyle = "#0077cc";
-  // ctx.fill();
-  
-  // // add text to the circle
-  // ctx.font = "18px sans-serif";
-  // ctx.fillStyle = "white";
-  // ctx.textAlign = "center";
-  // ctx.textBaseline = "middle";
-  // ctx.fillText("Step 2", 325, 75);
-}
-
-*/
