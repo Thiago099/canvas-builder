36315330b73d5022540cd10861c112b10cd27c5a
Thiago Kaique <thiago.kaique@stayfilm.com.br>
2022-12-18 20:16:02 -0300

    [feature]

--- src/canvas-builder/block/node.js
+++ src/canvas-builder/block/node.js
@@ -1,24 +1,36 @@
 import { canvas, surface, filler_surface } from "@/canvas-builder/canvas-builder";
 export default function node(canvas,makeInteractive,{inbound,outbound},{x,y},text)
 {
-    const height = (Math.max(inbound,outbound) * 50)+30 ;
-    const width = 150
+    const height = (Math.max(inbound.length,outbound.length) * 50)+30 ;
+    const width = 180
     var result = surface()
 
 
-    const padding_inbound = height/(inbound+1);
+    const padding_inbound = height/(inbound.length+1);
     result.data = {}
     result.data.points = []
-    for(var i = 0;i<inbound;i++)
+    for(var i = 0;i<inbound.length;i++)
     {
-        result.data.points.push({x:10,y:i*padding_inbound+padding_inbound+15})
+        result.data.points.push(
+            {
+                x:10,
+                y:i*padding_inbound+padding_inbound+15,
+                inbound:true,
+                text: inbound[i]
+            })
     }
     
-    const padding_outbound = height/(outbound+1); ;
+    const padding_outbound = height/(outbound.length+1); ;
     result.data.outbound_points = []
-    for(var i = 0;i<outbound;i++)
+    for(var i = 0;i<outbound.length;i++)
     {
-        result.data.points.push({x:width-10,y:i*padding_outbound+padding_outbound+15})
+        result.data.points.push(
+            {
+                x:width-10,
+                y:i*padding_outbound+padding_outbound+15,
+                inbound:false,
+                text: outbound[i]
+            })
     }
 
     result
@@ -34,19 +46,34 @@ export default function node(canvas,makeInteractive,{inbound,outbound},{x,y},tex
         ctx.roundRect(10,0,w-20,height,5);
         ctx.fill();
 
+
+
+
+        ctx.textBaseline = "middle";
+        ctx.font = "15px Arial";
+
+
         for(const point of result.data.points)
         {
             ctx.beginPath();
             ctx.fillStyle = "#00ff00";
             ctx.arc(point.x,point.y,10,0,2*Math.PI);
             ctx.fill();
+            ctx.fillStyle = "#ffffff";
+            if(point.inbound)
+            {
+                ctx.textAlign = "start";
+                ctx.fillText(point.text, point.x+15,point.y);
+            }
+            else
+            {
+                ctx.textAlign = "end";
+                ctx.fillText(point.text, point.x-15,point.y);
+            }
         }
-
-        ctx.fillStyle = "#ffffff";
         ctx.font = "20px Arial";
-        // text box
         ctx.textAlign = "center";
-        ctx.textBaseline = "middle";
+        ctx.fillStyle = "#ffffff";
         ctx.fillText(text, w/2, 25);
 
 
--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ import useBlock from "@/canvas-builder/block/block";
 var myc = canvas(cel);
 const node = useBlock(myc)
 
-const node1 = node({inbound:3,outbound:3},{x:10,y:10}, "Node 1")
-const node2 = node({inbound:3,outbound:1},{x:200,y:10},"Node 2")
+const node1 = node({inbound:["color","alpha"],outbound:["test"]},{x:10,y:10}, "Node 1")
+const node2 = node({inbound:["a","b","c"],outbound:["d"]},{x:250,y:10},"Node 2")
 
 myc.update();
